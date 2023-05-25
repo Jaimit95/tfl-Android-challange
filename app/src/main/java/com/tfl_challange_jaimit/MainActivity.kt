0b05b3cbaac5d0e081e8e4a7642c88000821f5c8
Jaimit Modi <jaimitmodi@gmail.com>
2023-05-25 15:41:56 +0100

    Initial Commit.

---
+++ app/src/main/java/com/tfl_challange_jaimit/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tfl_challange_jaimit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
